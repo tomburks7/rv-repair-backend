a0a7bf0639efbb06e81f9feb66d0edc3482b391e
tomburks7 <tomburks7@gmail.com>
2026-03-24 08:44:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = global.fetch;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 app.use(cors());
