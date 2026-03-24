05402b2365b3efa7e0cbfc61da049e2f2e8a2fdf
tomburks7 <tomburks7@gmail.com>
2026-03-24 08:01:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-import fetch from "node-fetch";
+const fetch = require("node-fetch");
 const app = express();
 app.use(cors());
 app.use(express.json());
